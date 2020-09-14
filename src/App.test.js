52f6e8500445b97eb9d79342b61be5de5d9cbf5c
Samantha Goodman <slgoodman89@gmail.com>
2020-09-14 19:00:34 -0400

    started added to test

--- src/App.test.js
+++ src/App.test.js
@@ -5,3 +5,15 @@ import App from "./App";
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+describe('user enters correct name', () => {
+  it('name input is Sam', () {
+  //arrange (setup)
+  let expected = 'Sam';
+  
+  //act (doing the thing)
+  
+  
+  //assert (having done the thing, did it do it right? / 'what could possibly fail?' )
+  });
+});
